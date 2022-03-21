3e693f85c62dc951b6e88a946240f9987ee30e0f
MacwithaQ <maqallaf@gmail.com>
2022-03-21 16:51:32 +0300

    edit database connection

--- database/database.js
+++ database/database.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
+  const DATABASE = process.env.DATABASE;
   const PASSWORD = process.env.PASSWORD;
-  // const CONNECTION_URL = `mongodb+srv://mkalansari:${PASSWORD}@coded.g97iy.mongodb.net/test?authSource=admin&replicaSet=atlas-pgw0fm-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
-  const CONNECTION_URL = `mongodb+srv://admin:${PASSWORD}@coded.9yoca.mongodb.net/mentoredTest?retryWrites=true&w=majority`;
+  const CONNECTION_URL = `mongodb+srv://mkalansari:${PASSWORD}@coded.g97iy.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
 
   const conn = await mongoose.connect(CONNECTION_URL, {
     useUnifiedTopology: true,
