be8dddf13defa09482cbd6a654a70db97d0d16da
MacwithaQ <maqallaf@gmail.com>
2022-03-28 13:11:16 +0300

    references in message and convo schemas

--- database/models/Conversation.js
+++ database/models/Conversation.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 // Creates a mongoose schema to define how a certain object is to be added to the database
 const ConversationSchema = new mongoose.Schema(
   {
-    members: {
-      type: Array,
-    },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
   },
   { timestamps: true }
 );
--- database/models/Message.js
+++ database/models/Message.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema(
   {
     conversationId: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
     },
     sender: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     text: {
       type: String,
